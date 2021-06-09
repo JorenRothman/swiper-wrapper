774a6aaaab22d173019bde2e64557daa4fbdbf4c
Joren Rothman <rothmanjoren@gmail.com>
2021-06-09 12:40:10 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,8 @@
-import SwiperCore, {
-    A11y,
-    Autoplay,
-    Controller,
-    EffectCoverflow,
-    EffectCube,
-    EffectFade,
-    EffectFlip,
-    HashNavigation,
-    History,
-    Keyboard,
-    Lazy,
-    Mousewheel,
-    Navigation,
-    Pagination,
-    Parallax,
-    Scrollbar,
-    Thumbs,
-    Virtual,
-    Zoom,
-} from 'swiper/core';
+import Swiper, { SwiperOptions } from 'swiper';
 
-import { SwiperOptions } from 'swiper';
 import createSwiperInstance from './createSwiperInstance';
-
-SwiperCore.use([
-    Virtual,
-    Keyboard,
-    Mousewheel,
-    Navigation,
-    Pagination,
-    Scrollbar,
-    Parallax,
-    Zoom,
-    Lazy,
-    Controller,
-    A11y,
-    History,
-    HashNavigation,
-    Autoplay,
-    EffectFade,
-    EffectCube,
-    EffectFlip,
-    EffectCoverflow,
-    Thumbs,
-]);
-
 interface ISwiperWrapperOptions {
+    swiper: Swiper;
     lazyInit: boolean;
 }
 
